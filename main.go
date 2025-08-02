676cbf104bb2d3714c1277e7eaf00063a57f4f3c
Bigesto <u.baratelli@gmail.com>
2025-08-02 09:33:03 +0200

    First commit. Adding proto-server.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
